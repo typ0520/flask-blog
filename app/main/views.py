1de0156a27296b97258a552e6774ea01d86a1c96
佟亚鹏 <yapeng.tong@baidao.comc>
2017-07-30 15:30:59 +0800

    fix-email-active

--- app/main/views.py
+++ app/main/views.py
@@ -3,11 +3,12 @@
 __author__ = 'typ0520'
 
 from datetime import datetime
-from flask import render_template,session, redirect, url_for
+from flask import render_template, session, redirect, url_for
 
 from . import main
 from .. import db
 from ..models import User
+
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
--- app/models.py
+++ app/models.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'typ0520'
 
-#python manage.py db init
-#python manage.py db migrate -m "initial migration"
-#python manage.py db upgrade
+# python manage.py db init
+# python manage.py db migrate -m "initial migration"
+# python manage.py db upgrade
 
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -13,56 +13,60 @@ from flask import current_app
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+
 class Role(db.Model):
-	__tablename__ = 'roles'
+    __tablename__ = 'roles'
 
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), unique=True)
-	users = db.relationship('User', backref='role')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
 
-	def __repr__(self):
-		return '<Role %r>' % self.name
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
 
 class User(UserMixin, db.Model):
-	__tablename__ = 'users'
+    __tablename__ = 'users'
 
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(64), unique=True, index=True)
-	username = db.Column(db.String(64), unique=True, index=True)
-	password_hash = db.Column(db.String(128))
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-	confirmed = db.Column(db.Boolean, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
 
-	def __repr__(self):
-		return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-	@property
-	def password(self):
-		raise AttributeError('password is not a readable attribute')
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
 
-	@password.setter
-	def password(self, password):
-		self.password_hash = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def verify_password(self, password):
-		return check_password_hash(self.password_hash, password)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-	def generate_confirmation_token(self, expiration=3600):
-		s = Serializer(current_app.config['SECRET_KEY'], expiration)
-		return s.dumps({'confim': self.id})
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        token = s.dumps({'confirm': self.id})
+        return token
 
-	def confirm(self, token):
-		s = Serializer(current_app.config['SECRET_KEY'])
-		try:
-			data = s.loads(token)
-		except:
-			return False
-		if data.get('confirm') != self.id:
-			return False
-		self.confirmed = True
-		sb.session.add(self)
-		return True
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
 
 @login_manager.user_loader
 def load_user(user_id):
-	return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
