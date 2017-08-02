7f02bb13f44e57ce0b8db5c37f419cd6d8ae3ba8
typ0520 <yapeng.tong@baidao.comc>
2017-08-02 16:04:18 +0800

    edit user info

--- app/main/views.py
+++ app/main/views.py
@@ -3,12 +3,13 @@
 __author__ = 'typ0520'
 
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, abort
-from flask_login import login_required
+from flask import render_template, session, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 from . import main
 from .. import db
-from ..models import User, Permission
+from ..models import User, Role, Permission
 from ..decorators import permission_required, admin_required
+from .forms import EditProfileForm, EditProfileAdminForm
 
 
 @main.route('/admin')
@@ -30,9 +31,54 @@ def for_moderator_only():
 def index():
     return render_template('index.html')
 
+
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
     return render_template('user.html', user=user);
+
+
+@main.route('/eidt-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has bean updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/eidt-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('Your profile has bean updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
