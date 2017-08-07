198898c2576962fb635e518dd32147b56cc3ba5f
typ0520 <yapeng.tong@baidao.comc>
2017-08-07 18:47:44 +0800

    Update README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+## flask-blog
+
+[![license](https://img.shields.io/hexpm/l/plug.svg)](https://github.com/typ0520/flask-blog/blob/master/LICENSE)
+
+Flask-blog is a micro-blog
+
+## Setup wizard
+
+```
+git clone https://github.com/typ0520/flask-blog.git
+cd flask-blog
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py runserver -h 0.0.0.0 -p 5000
+```
+
+## License
+flask-blog is under the Apache license. See the [LICENSE](https://github.com/typ0520/flask-blog/blob/master/LICENSE) file for details.
--- manage.py
+++ manage.py
@@ -8,21 +8,27 @@ from app.models import User, Role, Post, Permission, Comment, Follow
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+config_env = os.getenv('FLASK_CONFIG') or 'default'
+print('config_env: %s' % config_env)
+app = create_app(config_env)
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Permission=Permission, Role=Role, Post=Post, Follow=Follow, Comment=Comment)
+    return dict(app=app, db=db, User=User, Permission=Permission, Role=Role, Post=Post, Follow=Follow, Comment=Comment)
+
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def test():
-	import unittest
-	tests = unittest.TestLoader().discover('tests')
-	unittest.TextTestRunner(verbosity=2).run(tests)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
